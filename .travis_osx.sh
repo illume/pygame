0df077bc7516b13a67851c574d409ed1caa06ac3
Thomas Kluyver <thomas@kluyver.me.uk>
2016-06-26 15:50:59 +0100

    oops

--- .travis_osx.sh
+++ .travis_osx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 git clone https://github.com/MacPython/terryfy.git
-cd multibuild
+cd terryfy
 # Work with a specific commit
 git checkout c9800884ae1171bfc72b2737974f5e8dec28479d
 cd ..
